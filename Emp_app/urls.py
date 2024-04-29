c206b3ea2fef670af9ffdbfb842bae455a730f3c
AnjaliTiwari1005 <168248649+AnjaliTiwari1005@users.noreply.github.com>
2024-04-29 15:19:36 +0530

    Add files via upload

---
+++ Emp_app/urls.py
@@ -0,0 +1,22 @@
+"""
+URL configuration for Empolyee_info_proj project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views  # Ensure that your views are in the same directory as your urls.py or adjust the import accordingly.
+
+urlpatterns = [
+    path('', views.index1, name='index1'),  # This maps the root URL to the index1 view.
+]
---
+++ Emp_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index1(request):
+    return render(request, 'index1.html')
+
+
